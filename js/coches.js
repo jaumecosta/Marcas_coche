7b80e471b3f4e766fd0bffff78ded5ccb1fe455e
jaumecosta <jaumecostagomez@gmail.com>
2020-05-18 20:52:15 +0200

    consola ejercicio hecha

---
+++ js/coches.js
@@ -0,0 +1,11 @@
+       
+              var nombre = prompt('Escribe tu nombre: '); // primera interacción con el usuario
+              var marcas = [];
+              for (var i = 0; i <= 2; i++) {
+                var marca = prompt('Introduce tres marcas de coches que has tenido');
+                marcas.push(marca);
+                
+              }
+              console.log("Nombre: " + nombre +  " Marcas que ha usado: " + marcas);
+    
+  
